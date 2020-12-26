69e672d07cc7e0fe58562d3ac510c9dc63962fd1
Alan Ip <theipster@users.noreply.github.com>
2020-12-26 18:47:23 +0000

    🎨 Prettify container build output.

--- tests.sh
+++ tests.sh
@@ -4,19 +4,19 @@
 suite=`find . -type f -name *.test.sh | sort`
 
 # Begin
-echo "Running test suite: (`echo "$suite" | wc -l` found)"
-echo "===="
+echo "Test suite started (`echo "$suite" | wc -l` found) 🤞"
+echo "========"
 
 # Run tests (fail fast)
 for test in $suite; do
   dir=`dirname "$test"`
   file=`basename "$test"`
-  (cd "$dir" && echo -n "$dir: " && . "$file" && echo " ✔")
+  (cd "$dir" && echo -n "$dir: " && . "$file" && echo " ✅")
   if [ "$?" != 0 ]; then
     echo " ❌" && exit 1
   fi
 done
 
 # Done
-echo "===="
+echo "========"
 echo "Test suite complete. 👌"
